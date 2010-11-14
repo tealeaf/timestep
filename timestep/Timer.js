c0f058f4ab22a164455026c50552222e31d45e3d
Martin Hunt <mghunt@gmail.com>
2010-11-14 01:16:35 -0800

    long dts should be ignored since the computer was most likely asleep or the brwoser was paused at a breakpoint. we should probably increase this threshold... 5 minutes perhaps? it depends on the game type too. for multiplayer it should be about the timeout of the server connection; for single player, it can be relatively short since we're not syncing in-game time with other computers

--- timestep/Timer.js
+++ timestep/Timer.js
@@ -1,18 +1,39 @@
+var MAX_TICK = 10000; // ticks over 10 seconds will be considered too large to process
+
 exports.reset = function() { this._last = null; }
 exports.tick = function() {
 	var now = +new Date;
 	var ok = false;
 	try {
-		exports.onTick(-(exports._last || now) + (exports._last = now));
+		var dt = now - (exports._last || now);
+		
+		if (dt > MAX_TICK) {
+			exports.onLargeTick(dt, MAX_TICK);
+			dt = 1;
+		}
+		
+		exports._last = now;
+		exports.onTick(dt);
 		ok = true;
-	} 	
+	} 
 	finally {
 		if (exports.debug && !ok) {
 			app.stopLoop()
 		}
 	}
 }
-exports.onTick = function() {}
+
+/**
+ * If our computer falls asleep, dt might be an insanely large number. 
+ * If we're running a simulation of sorts, we don't want the computer
+ * to freeze while computing 1000s of simulation steps, so just drop
+ * this tick.  Anyone who is interested can listen for a call to 'onLargeTick'
+ */
+exports.onLargeTick = function(largeDt, threshold) {
+	logger.warn('Dropping large tick: ' + largeDt + '; Threshold is set at: ' + threshold);
+}
+
+exports.onTick = function(dt) {}
 
 exports.debug = false;
 
